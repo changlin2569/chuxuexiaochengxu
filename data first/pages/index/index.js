6444f4332f0276e75fa3974e3c9e250b5df0b5c3
lichanglinshan <1119785353@qq.com>
2020-09-24 21:28:02 +0800

    界面层和单向数据流

--- data first/pages/index/index.js
+++ data first/pages/index/index.js
@@ -3,12 +3,37 @@
 const app = getApp();
 
 Page({
-  date : {
+  data : {
     message : "hello world",
     jay : {
       name : '周杰伦',
       age : 18
     },
-    viewClassName : 'first'
+    viewClassName : 'first',
+    todos : [{
+      name : '数学',
+      checked : true
+    },{
+      name : '语文',
+      checked : false
+    },{
+      name : '英语',
+      checked : false
+    }]
+  },
+  // tapNameTwo : function(){
+  //   console.log('我是孩子');
+  // }
+  // tapName : function(e){
+  //   console.log(this);
+  // },
+  inputName:function(e){
+    // console.log(e.detail.value);
+    // this.data.message = e.detail.value;
+    // console.log(this);
+    this.setData({
+      message: e.detail.value
+    })
+    
   }
 })
