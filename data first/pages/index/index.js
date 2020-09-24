21e86ef4323e5575d4ae0f3ed0874b6f2a8185c7
lichanglinshan <1119785353@qq.com>
2020-09-24 17:40:46 +0800

    数据绑定bug

---
+++ data first/pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+const app = getApp();
+
+Page({
+  date : {
+    message : "hello world",
+    jay : {
+      name : '周杰伦',
+      age : 18
+    },
+    viewClassName : 'first'
+  }
+})
