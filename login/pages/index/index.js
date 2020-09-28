224ebab41bc495a60e7742baa2777bf12e26ef73
lichanglinshan <1119785353@qq.com>
2020-09-28 22:00:03 +0800

    小组件练习

--- login/pages/index/index.js
+++ login/pages/index/index.js
@@ -5,19 +5,39 @@ const app = getApp()
 Page({
   data: {
    username : 'jay',
-   userpassword : '12345678'
+   userpassword : '12345678',
+   show : true
   },
-  nameHandle : function(e){
+  // nameHandle : function(e){
+  //   this.setData({
+  //     username : e.detail.value
+  //   })
+  // },
+  // passwordHandle : function(e){
+  //   this.setData({
+  //     userpassword : e.detail.value
+  //   })
+  // },
+  inputHandle : function(e){
+    // console.log(e);
+    let propo = e.target.dataset['propo'];
+    let changed = {};
+    changed[propo] = e.detail.value;
+    this.setData(changed);
+    
+  },
+  loginhandle : function(e){
+    console.log(this.data);
+    //第三种form表单写法
+    console.log(e);//拿到界面层中的数据修改逻辑层
+    
+  },
+  showTime :function(){
+    // console.log(this);
     this.setData({
-      username : e.detail.value
+      show: !this.data.show
     })
-  },
-  passwordHandle : function(e){
-    this.setData({
-      userpassword : e.detail.value
-    })
-  },
-  loginhandle : function(){
-    console.log(this.data); 
+    console.log(this.data.show);
+    
   }
 })
